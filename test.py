8abaeb10ce39f4da3cba7d18ee00110c95ea4b86
quesooo <wangulen07@gmail.com>
2016-10-11 19:24:40 -0300

    Agregado otro ejemplo

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import timeQ
+from random import randint
 
 print timeQ.monotonic_time()
+
+tInicial=timeQ.monotonic_time()
+i=0
+while i < 1000000: #Contar desde 0 a 10e10
+	i+=1
+print "tiempo en contar 10e6"
+print timeQ.monotonic_time()-tInicial
